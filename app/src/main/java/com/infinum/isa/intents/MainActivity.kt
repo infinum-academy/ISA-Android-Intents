f7b710293471eebe819293004eafcfaf85e8ea19
Filip Floreani <filip.floreani@infinum.hr>
2021-07-06 15:49:54 +0200

    Initial commit

---
+++ app/src/main/java/com/infinum/isa/intents/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.infinum.isa.intents
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.infinum.isa.intents.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // TODO: Define binding variable
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Inflate binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // TODO: Set text
+        setAcademyTitle()
+        // TODO: Set click listener
+        setPressMeClickListener()
+    }
+
+    private fun setAcademyTitle() {
+        binding.helloLabel.text = "Djevojka sa sela s mojim štiklama"
+    }
+
+    private fun setPressMeClickListener() {
+        binding.implicitIntentButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                // TODO: Do something implicit
+                startValidUrlIntent()
+            }
+        })
+
+        binding.explicitIntentButton.setOnClickListener {
+                // TODO: Start explicit intent (but to where? create a new activity!)
+            startExplicitIntent()
+        }
+    }
+
+    private fun startInvalidUrlIntent() {
+        val uri = Uri.parse("ovo_nece://radit")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+
+        // This will go boom with an ActivityNotFoundException
+        startActivity(intent)
+    }
+
+    private fun startValidUrlIntent() {
+        try {
+            val uri = Uri.parse("https://academy.infinum.com")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            startActivity(intent)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(this, "Nismo uspjeli otvoriti link", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun startExplicitIntent() {
+        val intent = TheOtherActivity.buildIntent(
+            originActivity = this,
+            userName = binding.usernameInput.text.toString(),
+            passwordLength = binding.passwordInput.text?.toString().orEmpty().length
+        )
+
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/infinum/isa/intents/TheOtherActivity.kt
@@ -0,0 +1,48 @@
+package com.infinum.isa.intents
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.infinum.isa.intents.databinding.ActivityTheOtherBinding
+
+class TheOtherActivity : AppCompatActivity() {
+
+    companion object {
+        private const val EXTRA_USERNAME = "EXTRA_USERNAME"
+        private const val EXTRA_PASSWORD_LENGTH = "EXTRA_PASSWORD_LENGTH"
+
+        fun buildIntent(originActivity: Activity, userName: String, passwordLength: Int): Intent {
+            val intent = Intent(originActivity, TheOtherActivity::class.java)
+            intent.putExtra(EXTRA_USERNAME, userName)
+            intent.putExtra(EXTRA_PASSWORD_LENGTH, passwordLength)
+
+            return intent
+        }
+    }
+
+    private lateinit var binding: ActivityTheOtherBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityTheOtherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val userName = intent.extras?.getString(EXTRA_USERNAME)
+        val passwordLength = intent.extras?.getInt(EXTRA_PASSWORD_LENGTH)
+
+        setData(userName, passwordLength)
+    }
+
+    private fun setData(userName: String?, passwordLength: Int?) {
+        if (userName != null) {
+            binding.usernameLabel.text = userName
+        }
+
+        if (passwordLength != null) {
+            binding.passwordLengthLabel.text = "Password length: $passwordLength"
+        }
+    }
+}
