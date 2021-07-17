63fc917456d6a9884a73001ca71e339ca43da764
BraneRi <branimir.akmadza@infinum.hr>
2021-07-18 00:21:31 +0200

    Add permission helper functions

--- app/src/main/java/com/infinum/isa/playground/lecturefive/PermissionExtensions.kt
+++ app/src/main/java/com/infinum/isa/playground/lecturefive/PermissionExtensions.kt
@@ -1,7 +1,43 @@
 package com.infinum.isa.playground.lecturefive
 
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.snackbar.Snackbar
+import com.infinum.isa.playground.R
+
 //TODO create extension for requesting permissions
 
-//TODO create function to handle denied permission
+/**
+ * Displays the default Snackbar with Action to open the Application Details
+ * in Settings so the user can enable permissions.
+ */
+private fun showPermissionsDeniedSnackbar(
+    fragmentActivity: FragmentActivity,
+    message: String = fragmentActivity.getString(R.string.no_permission)
+) {
+    fragmentActivity.apply {
+        Snackbar.make(
+            findViewById(android.R.id.content),
+            message,
+            Snackbar.LENGTH_LONG
+        )
+            .setAction(
+                R.string.settings
+            ) { startActivity(getApplicationSettingsIntent(this)) }.show()
+    }
+}
 
-//TODO create function for settings intent
+/**
+ * Creates an intent for the application details in settings.
+ */
+private fun getApplicationSettingsIntent(
+    fragmentActivity: FragmentActivity
+): Intent {
+    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+    intent.data = Uri.parse("package:${fragmentActivity.packageName}")
+    return intent
+}
