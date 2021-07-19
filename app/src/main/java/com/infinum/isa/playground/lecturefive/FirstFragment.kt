cc47f60f42c895bb94991701811895b0fa5fdbfe
BraneRi <branimir.akmadza@infinum.hr>
2021-07-19 16:08:35 +0200

    Remove debug toast

--- app/src/main/java/com/infinum/isa/playground/lecturefive/FirstFragment.kt
+++ app/src/main/java/com/infinum/isa/playground/lecturefive/FirstFragment.kt
@@ -1,11 +1,9 @@
 package com.infinum.isa.playground.lecturefive
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.infinum.isa.playground.databinding.FragmentFirstBinding
@@ -13,6 +11,7 @@ import com.infinum.isa.playground.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -44,6 +43,5 @@ class FirstFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        Toast.makeText(context, "FLO: $this", Toast.LENGTH_LONG).show()
     }
 }
